816ce4a15be01d344e8de6694554d779638b6fb3
ika25 <lomiika1990@yahoo.com>
2020-12-01 09:50:38 +0000

    Level Design - The Buildings animation added

---
+++ Endless Runner 3.0/Assets/Scripts/BuildingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingController : MonoBehaviour
+{
+    private Transform player;
+    private Animator animator;
+    private bool hasAppeared = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerController").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() // in here we need to calculate distance between the gameObject and player
+    {
+        if (Vector3.Distance(transform.position, player.position) < 200 && !hasAppeared) ;
+        {
+            animator.SetTrigger("Appear");
+            hasAppeared = true;
+        }
+    }
+}
