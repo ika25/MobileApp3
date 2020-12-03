0f23f2127be2450ce9311e0ac28d5db793ec77da
ika25 <lomiika1990@yahoo.com>
2020-12-03 13:28:43 +0000

    car collider added

---
+++ Endless Runner 3.0/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)//kill the player ones colider has entered player
+    {
+        if (other.collider.tag == "Player")// check if other 
+        {
+            if (!other.collider.GetComponent<PlayerController>().IsDead) //check if player is not dead
+            {
+                other.collider.GetComponent<PlayerController>().KillPlayer();// then kill the player
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
