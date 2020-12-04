4982e3428e6e1cc4ca595bca75fdc039e1b5573a
ika25 <lomiika1990@yahoo.com>
2020-12-04 14:04:51 +0000

    added heart(health) script

---
+++ Endless Runner 3.0/Assets/Scripts/Heart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    public GameObject Particle_Heart;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")// lets check if this is player or not
+        {
+            Instantiate(Particle_Heart, transform.position, Quaternion.identity);// instantiate effect
+            Destroy(gameObject);//destroy game objet heart
+
+        }
+    }
+}
