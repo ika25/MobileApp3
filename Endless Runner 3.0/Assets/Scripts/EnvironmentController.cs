af7e9babf6759359fb1f4c46cc8953150cc41688
ika25 <lomiika1990@yahoo.com>
2020-12-05 14:22:48 +0000

    infinity level added (fixed pivot issue)

---
+++ Endless Runner 3.0/Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+    public GameObject newStreet;//prefab we created
+    public GameObject currentStreet;//
+
+    private GameObject oldStreet;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //here we need to check if player has entered start or middle 
+        if (other.tag == "Start")
+        {
+            if (oldStreet != null)
+            {
+                Destroy(oldStreet);//destroy old street
+            }
+            else
+            {
+                Destroy(GameObject.Find("MainScene"));//spawn new level
+            }
+        }
+        else if (other.tag == "Middle")
+        {
+            SpawnLevel();
+        }
+    }
+    void SpawnLevel()
+    {
+        oldStreet = currentStreet;//assign street
+        currentStreet = (GameObject)Instantiate(newStreet, currentStreet.transform.GetChild(9).position, Quaternion.identity);
+
+    }
+}
