8a47279a84fb8652db6ce215944a1b912d3f2c8c
ika25 <lomiika1990@yahoo.com>
2020-12-05 14:54:34 +0000

    stwitching between camers (could be bit smoother)

---
+++ Endless Runner 3.0/Assets/Scripts/LevelStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStart : MonoBehaviour
+{
+    //check function that will be called when the player has entered the collider of this object
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(GameObject.Find("Main Camera").gameObject);// removed the main camera
+        }
+    }
+}
