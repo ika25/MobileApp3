f53975f1d8e00400c629a14e3cd754fec03ae1e7
ika25 <lomiika1990@yahoo.com>
2020-12-10 19:44:58 +0000

    Developer Canvas and Help Canvas (finally working right)

---
+++ Endless Runner 3.0/Assets/Scripts/Developer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Developer : MonoBehaviour
+{
+    public bool show = true;
+    public Help help;
+
+    // Update is called once per frame
+    public void Show_Hide()
+    {
+        if (show)
+        {
+            if (!help.show)
+            {
+                help.gameObject.GetComponent<Animator>().SetTrigger("Hide");
+                help.show = true;
+            }
+            
+            GetComponent<Animator>().SetTrigger("Show");
+            show = false;
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Hide");
+            show = true;
+        }
+    }
+}
+
---
+++ Endless Runner 3.0/Assets/Scripts/Help.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Help : MonoBehaviour
+{
+    public bool show = true;
+    public Developer dev;
+
+    // Update is called once per frame
+    public void Show_Hide()
+    {
+        if (show)
+        {
+            if (!dev.show)
+            {
+                dev.gameObject.GetComponent<Animator>().SetTrigger("Hide");
+                dev.show = true;
+            }
+            
+            GetComponent<Animator>().SetTrigger("Show");
+            show = false;
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Hide");
+            show = true;
+        }
+    }
+}
