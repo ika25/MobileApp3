f6722d7c14bf914cfb247a027f61541a7ba9bab0
ika25 <lomiika1990@yahoo.com>
2020-12-10 20:27:23 +0000

    Pause Menu added

---
+++ Endless Runner 3.0/Assets/Scripts/Menus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menus : MonoBehaviour
+{
+    // Veriables
+    public PlayerController player;
+    public GameObject PauseMenu;
+
+    // Function that will pause the game
+    public void PauseGame()
+    {
+        if (!player.IsDead)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    // 1function to resume the game
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        
+    }
+
+    // Function when clicked on main menu to reload the current scene
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+
+    // Function to quit
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
